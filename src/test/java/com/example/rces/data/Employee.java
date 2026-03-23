dec505b698751e321fe25d163c715668e469b6e8
ByteCodeAPAAPA <ilya26.khohlov@gmail.com>
2026-03-23 11:47:53 +0300

    #6 allure

--- src/test/java/com/example/rces/data/Employee.java
+++ src/test/java/com/example/rces/data/Employee.java
@@ -1,15 +1,16 @@
 package com.example.rces.data;
 
 import com.example.rces.dto.EmployeeDTO;
-import com.example.rces.dto.SubDivisionDTO;
 import net.datafaker.Faker;
 
 import java.util.Locale;
 
+import static com.example.rces.data.SubDivisionTestHelper.empty_subDivision;
+
 public class Employee {
 
     public static final EmployeeDTO admin_user = new EmployeeDTO(
-            1L, "admin", SubDivision.empty_subDivision, "ADMIN", true, -1L
+            1L, "admin", empty_subDivision, "ADMIN", true, -1L
     );
 
     private static final Faker faker = new Faker(new Locale("en"));
@@ -17,7 +18,7 @@ public class Employee {
     public static EmployeeDTO employeeGenerator() {
         return EmployeeDTO.builder()
                 .name(faker.name().firstName() + " " + faker.name().lastName())
-                .subDivision(SubDivision.empty_subDivision)
+                .subDivision(empty_subDivision)
                 .role("USER")
                 .isActive(true)
                 .chatId(-1L)
